b4814dd53371ecb1b8b1f07b6ff2720a4e92d017
Akash Sahu <sahuakash85850@gmail.com>
2025-06-28 16:13:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let input = document.querySelector(".inputbox");
+
+let btn = document.querySelectorAll(".buttons");
+
+let string = "";
+
+let arr = Array.from(btn);
+arr.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.slice(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
